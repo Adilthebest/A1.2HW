d939274da35f5dc24a4536e4a478306df3297600
Hp <adilthebestw27@gmail.com>
2022-04-15 17:26:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson2classwork/FifthFragment.java
@@ -0,0 +1,33 @@
+package com.example.lesson2classwork;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lesson2classwork.databinding.FragmentFifthBinding;
+
+
+public class FifthFragment extends Fragment {
+private FragmentFifthBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding= FragmentFifthBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        String text = getArguments().getString("key4");
+        binding.textFifth.setText(text);
+
+    }
+}
